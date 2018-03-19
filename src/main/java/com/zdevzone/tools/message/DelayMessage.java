b044eddabb04a327a67b2fb17a21cd7b01cca954
zhengweibao <zhengweibao@gomo.com>
2018-03-19 18:14:52 +0800

    更改项目名称

--- src/main/java/com/zdevzone/tools/message/DelayMessage.java
+++ src/main/java/com/zdevzone/tools/message/DelayMessage.java
@@ -27,8 +27,10 @@ public class DelayMessage{
 	 * 大约为40+天左右， 而项目中使用的amqpTemplate中定义的delayTime的数据类型为int，
 	 * 在JAVA中int类型的最大值只有2^31 - 1, 换算为天数大约为20+
 	 * 因此为了避免数值溢出和实现长时间的延时消息发送，这里采用了接力的形式，
-	 * 当延时消息延时间隔大于指定值({@code me.zhengweibao.utils.config.DelayMessageConfiguration#MAX_DELAY_MESSAGE_TIME}, 15天)
+	 * 当延时消息延时间隔大于指定值(15天)
 	 * 将以接力的形式发送该延时消息
+	 *
+	 * @see com.zdevzone.tools.config.DelayMessageConfiguration#MAX_DELAY_MESSAGE_TIME
 	 */
 	private boolean relayed;
 
